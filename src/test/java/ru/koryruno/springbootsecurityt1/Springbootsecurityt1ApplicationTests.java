61508427ca93228697625461ee127563ec27d280
koryruno <koryruno@yandex.ru>
2024-08-17 18:28:20 +0300

    Initialize project

---
+++ src/test/java/ru/koryruno/springbootsecurityt1/Springbootsecurityt1ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.koryruno.springbootsecurityt1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springbootsecurityt1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
